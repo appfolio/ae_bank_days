bf1cf66229a5dc4e8c9d41a13c7a8b8af0dabf93
Steven Boyd <steven.boyd@appfolio.com>
2020-07-07 16:16:32 -0700

    sb - use dates instead of times in the tests

--- test/ae_bank_days/bank_day_helper_test.rb
+++ test/ae_bank_days/bank_day_helper_test.rb
@@ -11,18 +11,15 @@ module AeBankDays
     end
 
     def test_bank_day__weekday
-      time = Time.parse('2017-10-03 14:30:45 -0500')
-      assert BankDayHelper.bank_day?(time.to_date)
+      assert BankDayHelper.bank_day?(Date.civil(2017, 10, 3))
     end
 
     def test_bank_day__weekend
-      time = Time.parse('2017-10-14 14:30:45 -0500')
-      refute BankDayHelper.bank_day?(time.to_date)
+      refute BankDayHelper.bank_day?(Date.civil(2017, 10, 14))
     end
 
     def test_bank_day__holiday
-      time = Time.parse('2017-10-09 14:30:45 -0500')
-      refute BankDayHelper.bank_day?(time.to_date)
+      refute BankDayHelper.bank_day?(Date.civil(2017, 10, 9))
     end
 
     def test_bank_day__saturday_holiday_is_not_observed_on_friday
@@ -30,68 +27,55 @@ module AeBankDays
       # This test makes sure that these dates are not accidentally marked as holidays.
       # July 4, 2020 fell on a Saturday. The day before was a generally observed federal holiday, but it was still
       # a banking day due to the federal banks holiday rules.
-      time = Time.parse('2020-07-03 14:30:45 -0500')
-      assert BankDayHelper.bank_day?(time.to_date)
+      assert BankDayHelper.bank_day?(Date.civil(2020, 6, 3))
     end
 
     def test_next_banking_day__weekday
-      time = Time.parse('2017-10-03 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-03'), BankDayHelper.next_banking_day(time.to_date)
+      assert_equal Date.civil(2017, 10, 3), BankDayHelper.next_banking_day(Date.civil(2017, 10, 3))
     end
 
     def test_next_banking_day__weekday__extra_days
-      time = Time.parse('2017-10-03 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-06'), BankDayHelper.next_banking_day(time.to_date, number_of_days: 3)
+      assert_equal Date.civil(2017, 10, 6), BankDayHelper.next_banking_day(Date.civil(2017, 10, 3), number_of_days: 3)
     end
 
     def test_next_banking_day__weekend
-      time = Time.parse('2017-10-14 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-16'), BankDayHelper.next_banking_day(time.to_date)
+      assert_equal Date.civil(2017, 10, 16), BankDayHelper.next_banking_day(Date.civil(2017, 10, 14))
     end
 
     def test_next_banking_day__weekend__extra_days
-      time = Time.parse('2017-10-14 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-19'), BankDayHelper.next_banking_day(time.to_date, number_of_days: 3)
+      assert_equal Date.civil(2017, 10, 19), BankDayHelper.next_banking_day(Date.civil(2017, 10, 14), number_of_days: 3)
     end
 
     def test_next_banking_day__holiday
-      time = Time.parse('2017-10-09 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-10'), BankDayHelper.next_banking_day(time.to_date)
+      assert_equal Date.civil(2017, 10, 10), BankDayHelper.next_banking_day(Date.civil(2017, 10, 9))
     end
 
     def test_next_banking_day__holiday__extra_days
-      time = Time.parse('2017-10-09 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-13'), BankDayHelper.next_banking_day(time.to_date, number_of_days: 3)
+      assert_equal Date.civil(2017, 10, 13), BankDayHelper.next_banking_day(Date.civil(2017, 10, 9), number_of_days: 3)
     end
 
     def test_previous_banking_day__weekday
-      time = Time.parse('2017-10-03 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-03'), BankDayHelper.previous_banking_day(time.to_date)
+      assert_equal Date.civil(2017, 10, 3), BankDayHelper.previous_banking_day(Date.civil(2017, 10, 3))
     end
 
     def test_previous_banking_day__weekday__extra_days
-      time = Time.parse('2017-10-06 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-03'), BankDayHelper.previous_banking_day(time.to_date, number_of_days: 3)
+      assert_equal Date.civil(2017, 10, 3), BankDayHelper.previous_banking_day(Date.civil(2017, 10, 6), number_of_days: 3)
     end
 
     def test_previous_banking_day__weekend
-      time = Time.parse('2017-10-14 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-13'), BankDayHelper.previous_banking_day(time.to_date)
+      assert_equal Date.civil(2017, 10, 13), BankDayHelper.previous_banking_day(Date.civil(2017, 10, 14))
     end
 
     def test_previous_banking_day__weekend__extra_days
-      time = Time.parse('2017-10-14 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-10'), BankDayHelper.previous_banking_day(time.to_date, number_of_days: 3)
+      assert_equal Date.civil(2017, 10, 10), BankDayHelper.previous_banking_day(Date.civil(2017, 10, 14), number_of_days: 3)
     end
 
     def test_previous_banking_day__holiday
-      time = Time.parse('2017-10-09 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-06'), BankDayHelper.previous_banking_day(time.to_date)
+      assert_equal Date.civil(2017, 10, 6), BankDayHelper.previous_banking_day(Date.civil(2017, 10, 9))
     end
 
     def test_previous_banking_day__holiday__extra_days
-      time = Time.parse('2017-10-09 14:30:45 -0500')
-      assert_equal Date.parse('2017-10-03'), BankDayHelper.previous_banking_day(time.to_date, number_of_days: 3)
+      assert_equal Date.civil(2017, 10, 3), BankDayHelper.previous_banking_day(Date.civil(2017, 10, 9), number_of_days: 3)
     end
   end
 end
